1a1fec35b5bbf9e40e32770293c828d124d3e1fd
basement_mustard <120338237+BasementMustard@users.noreply.github.com>
2023-08-15 13:07:09 -0600

    Create viruscheck.rb

---
+++ viruscheck.rb
@@ -0,0 +1,7 @@
+virusdetected = true
+
+if virusdetected
+  puts "ERROR: A virus has been detected. Delete this app right now and visit your local tech repair store."
+else
+  puts "No unsuspecting malware was detected."
+end
